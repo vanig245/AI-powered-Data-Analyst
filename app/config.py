2a7ec04885eeac4f7b21f803d08ae0e71ab9ece8
vanig245 <vani.gupta2405@gmail.com>
2026-07-30 12:46:15 +0530

    testing

--- app/config.py
+++ app/config.py
@@ -1,5 +1,5 @@
 import os
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     """
@@ -17,10 +17,11 @@ class Settings(BaseSettings):
     BASE_DIR: str = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     DATA_DIR: str = os.path.join(BASE_DIR, "sample_data")
 
-    class Config:
-        env_file = ".env"
-        env_file_encoding = 'utf-8'
-        case_sensitive = True
+model_config = SettingsConfigDict(
+    env_file=".env",
+    env_file_encoding='utf-8',
+    case_sensitive=True
+    )
 
 settings = Settings()
 

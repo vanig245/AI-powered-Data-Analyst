f025f9e4a7f5333e2189fd0e9968182a6659909f
vanig245 <vani.gupta2405@gmail.com>
2026-07-30 09:35:26 +0530

    added config.py in which there is API Key actual API Key placed in .env which is protected by .gitignore

---
+++ app/config.py
@@ -0,0 +1,27 @@
+import os
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    """
+    Application settings and environment variables.
+    Pydantic will automatically read these from a .env file or system environment variables.
+    """
+
+    GROQ_API_KEY: str = "GROQ_API_KEY"
+    GROQ_MODEL_NAME: str = "llama3-70b-8192"
+
+    APP_NAME: str = "AI Data Analyst"
+    APP_VERSION: str = "1.0.0"
+    DEBUG_MODE: bool = True
+
+    BASE_DIR: str = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    DATA_DIR: str = os.path.join(BASE_DIR, "sample_data")
+
+    class Config:
+        env_file = ".env"
+        env_file_encoding = 'utf-8'
+        case_sensitive = True
+
+settings = Settings()
+
+os.makedirs(settings.DATA_DIR, exist_ok=True)
